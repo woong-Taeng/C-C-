5441d784ff4a1d9badaedfa9bfba2fdcd4d38e3a
woong-Taeng <antonto04@gmail.com>
2026-09-15 12:33:57 +0900

    Add files via upload

---
+++ week_2/year.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main()
+{
+    int year;
+
+    printf("연도를 입력하세요 : ");
+    scanf("%d", &year);
+
+    if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0) {
+        printf("윤년입니다\n");
+    }
+    else {
+        printf("평년입니다\n");
+    }
+
+
+    return 0;
+}
